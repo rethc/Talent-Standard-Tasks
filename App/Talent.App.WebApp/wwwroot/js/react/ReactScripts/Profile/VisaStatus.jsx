dffa0d7e04197d2f81b7fbbfe8b9f821d4a409c1
rethc <reth@xtra.co.nz>
2020-12-14 23:53:46 +1300

    Created UI + boilerplate code for VisaStatus comp.

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/VisaStatus.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/VisaStatus.jsx
@@ -1,10 +1,70 @@
 ﻿import React from "react";
 import { SingleInput } from "../Form/SingleInput.jsx";
+import DatePicker from "react-datepicker";
+import Moment from "moment";
 
 export default class VisaStatus extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  render() {}
+  renderExpiryDate() {
+    if (
+      this.props.visaStatus == "Work Visa" ||
+      this.props.visaStatus == "Student Visa"
+    ) {
+      return (
+        <div>
+          <label>Visa expiry date</label>
+          <br />
+          <DatePicker
+            dateFormat="DD/MM/YYYY"
+            name="expiry"
+            selected={
+              Moment(this.props.visaExpiryDate)
+                ? Moment(this.props.visaExpiryDate)
+                : Moment()
+            }
+            onChange={this.handleChangeExpiryDate}
+          />
+        </div>
+      );
+    } else {
+      return <div />;
+    }
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="six wide column">
+          <label>Visa type</label>
+          <select
+            className="ui right labeled dropdown"
+            value={this.props.visaStatus ? this.props.visaStatus : ""}
+            onChange={this.updateContact}
+            name="visaStatus"
+          >
+            <option value="" disabled hidden>
+              Visa type
+            </option>
+            <option value="Citizen">Citizen</option>
+            <option value="Permanent Resident">Permanent Resident</option>
+            <option value="Work Visa">Work Visa</option>
+            <option value="Student Visa">Student Visa</option>
+          </select>
+        </div>
+        <div className="six wide column">{this.renderExpiryDate()}</div>
+        <div className="four wide column">
+          <button
+            type="button"
+            className="ui teal button"
+            onClick={this.saveContact}
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
