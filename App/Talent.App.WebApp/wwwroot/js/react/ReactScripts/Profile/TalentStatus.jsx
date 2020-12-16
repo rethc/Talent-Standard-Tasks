2856f47050da8e6c97f97f7fbb621fe7d1a34c61
rethc <reth@xtra.co.nz>
2020-12-16 23:49:59 +1300

    renderStatus() checkboxes

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/TalentStatus.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/TalentStatus.jsx
@@ -7,11 +7,53 @@ export default class TalentStatus extends React.Component {
     super(props);
   }
 
+  renderStatus() {
+    return (
+      <div>
+        <Form.Field>
+          <label>Current Status</label>
+        </Form.Field>
+        <Form.Field>
+          <Checkbox
+            radio
+            label="Actively looking for a job"
+            value="1"
+            onChange={this.handleChange}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox
+            radio
+            label="Not looking for a job at the moment"
+            value="2"
+            onChange={this.handleChange}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox
+            radio
+            label="Currently employed but open to offers"
+            value="3"
+            onChange={this.handleChange}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox
+            radio
+            label="Will be available on later date"
+            value="4"
+            onChange={this.handleChange}
+          />
+        </Form.Field>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="ui grid">
         <div className="row">
-          <div className="sixteen wide column">Test</div>
+          <div className="sixteen wide column">{this.renderStatus()}</div>
         </div>
       </div>
     );
