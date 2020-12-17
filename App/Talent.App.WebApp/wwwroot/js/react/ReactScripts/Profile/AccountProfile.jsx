c8e872760c12046fa55db1f4bef8c6d26a2ddc56
rethc <reth@xtra.co.nz>
2020-12-17 21:45:34 +1300

    PhotoUpload boilerplate

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/AccountProfile.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/AccountProfile.jsx
@@ -262,7 +262,7 @@ export default class AccountProfile extends React.Component {
                         saveProfileData={this.updateAndSaveData}
                       />
                     </FormItemWrapper>
-                    {/**
+
                     <FormItemWrapper
                       title="Profile Photo"
                       tooltip="Please upload your profile photo"
@@ -274,6 +274,7 @@ export default class AccountProfile extends React.Component {
                         savePhotoUrl="http://localhost:60290/profile/profile/updateProfilePhoto"
                       />
                     </FormItemWrapper>
+                    {/**
                     <FormItemWrapper
                       title="Profile Video"
                       tooltip="Upload a brief self-introduction video"
--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/PhotoUpload.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/PhotoUpload.jsx
@@ -1,11 +1,18 @@
-﻿/* Photo upload section */
-import React, { Component } from "react";
+﻿import React, { Component } from "react";
 import Cookies from "js-cookie";
+import { Icon, Image, Form, Button } from "semantic-ui-react";
 
 export default class PhotoUpload extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      uploaded: true,
+      uploading: false,
+    };
   }
 
-  render() {}
+  render() {
+    return <div className="ui sixteen wide column"></div>;
+  }
 }
