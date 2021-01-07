11fb9f774ffd6ff9de2d5f897ecad714e15ee9c0
rethc <reth@xtra.co.nz>
2021-01-08 01:18:57 +1300

    location error handling

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/CompanyProfile.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/CompanyProfile.jsx
@@ -5,9 +5,26 @@ export default class CompanyProfile extends React.Component {
   constructor(props) {
     super(props);
   }
-
   render() {
     let location = "No location found";
+    if (this.props.details) {
+      if (
+        this.props.details.companyContact.location.city &&
+        this.props.details.companyContact.location.country
+      )
+        location =
+          this.props.details.companyContact.location.city +
+          ", " +
+          this.props.details.companyContact.location.country;
+      else if (
+        !this.props.details.companyContact.location.city &&
+        this.props.details.companyContact.location.country
+      )
+        location = this.props.details.companyContact.location.country;
+      else {
+        location = this.props.details.companyContact.location.city;
+      }
+    }
 
     return (
       <Card>
@@ -15,9 +32,11 @@ export default class CompanyProfile extends React.Component {
           <Card.Header>
             {this.props.details ? this.props.details.companyContact.name : ""}
           </Card.Header>
-          <Card.Meta>{location}</Card.Meta>
+          <Card.Meta>
+            <Icon name="marker" /> {location}
+          </Card.Meta>
           <Card.Description>
-            We currently do not have specific skills that we desire
+            We currently do not have specific skills that we desire.
           </Card.Description>
         </Card.Content>
         <Card.Content extra>
