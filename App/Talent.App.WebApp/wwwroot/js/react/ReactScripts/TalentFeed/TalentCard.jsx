4121283561ac144998b50736b6d80424b6f71ffb
rethc <reth@xtra.co.nz>
2021-01-10 00:34:07 +1300

    header for talentcard

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentCard.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentCard.jsx
@@ -1,7 +1,8 @@
 ﻿import React from "react";
 import ReactPlayer from "react-player";
 import PropTypes from "prop-types";
-import { Popup, Icon } from "semantic-ui-react";
+import { Popup, Icon, Card, Button, Grid, Segment } from "semantic-ui-react";
+import { BodyWrapper, loaderData } from "../Layout/BodyWrapper.jsx";
 
 export default class TalentCard extends React.Component {
   constructor(props) {
@@ -10,9 +11,38 @@ export default class TalentCard extends React.Component {
     let loader = loaderData;
     loader.allowedUsers.push("Employer");
     loader.allowedUsers.push("Recruiter");
+
+    this.init = this.init.bind(this);
   }
 
+  init() {
+    let loaderData = TalentUtil.deepCopy(this.state.loaderData);
+    loaderData.isLoading = false;
+    this.setState({ loaderData });
+  }
+
+  componentDidMount() {}
+
   render() {
-    return <h1>hello from talent card</h1>;
+    const header = (
+      <React.Fragment>
+        <Grid columns="equal">
+          <Grid.Row>
+            <Grid.Column>
+              <h3>Name</h3>
+            </Grid.Column>
+            <Grid.Column textAlign="right">
+              <Icon name="star" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </React.Fragment>
+    );
+    return (
+      <Card fluid>
+        <Card.Content header={header} />
+        <Card.Content description>dfdsf</Card.Content>
+      </Card>
+    );
   }
 }
