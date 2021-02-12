2975dc692e917d17b56ab2780a47a61648f6c33e
rethc <reth@xtra.co.nz>
2021-02-13 03:54:16 +1300

    fix url

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentFeed.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentFeed.jsx
@@ -45,7 +45,8 @@ export default class TalentFeed extends React.Component {
   loadEmployerData() {
     var cookies = Cookies.get("talentAuthToken");
     $.ajax({
-      url: "http://localhost:60290/profile/profile/getEmployerProfile",
+      url:
+        "https://cprofileservices2.azurewebsites.net/profile/profile/getEmployerProfile",
       headers: {
         Authorization: "Bearer " + cookies,
         "Content-Type": "application/json",
@@ -93,7 +94,8 @@ export default class TalentFeed extends React.Component {
   loadTalentData() {
     var cookies = Cookies.get("talentAuthToken");
     $.ajax({
-      url: "http://localhost:60290/profile/profile/getTalent",
+      url:
+        "https://cprofileservices2.azurewebsites.net/profile/profile/getTalent",
       headers: {
         Authorization: "Bearer " + cookies,
         "Content-Type": "application/json",
